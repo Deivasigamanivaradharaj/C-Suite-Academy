fe1800037cab573cc2d3ed2e169c9c5a2df53ba6
Ramkumar-Rock <always.ramkumar7@gmail.com>
2024-05-29 19:01:22 +0530

    assessment ela parts compeleted boss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 //Bootstrap imported
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 
-
+//react-router
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+//Landingpage
 import Landingpage from './Components/Landingpage/Landingpage';
 import Management from './Components/Managements/Management';
 import Brands from './Components/Brands/Brands';
@@ -16,7 +17,12 @@ import FindPricing from './Components/FindPricing/FindPricing';
 import Partners from './Components/Partners/Partners';
 import Success from './Components/Success/Success';
 import Engage from './Components/Engage/Engage'
-import Overlaycards from './Components/Overlaycards/Overlaycards'
+// import Overlaycards from './Components/Overlaycards/Overlaycards'
+
+//Assessments
+import Entrylevel from './Components/Assessments/Entrylevel/Entrylevel';
+import Closelevel from './Components/Assessments/Closelevel/Closelevel';
+import Assessmentsstart from './Components/Assessments/Assessmentsstart/Assessmentsstart'
 
 function App() {
   return (
@@ -24,8 +30,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={[<Landingpage/>,<Management/>,<Brands/>,<Success/>,<Engage/>,<Partners/>,<FindPricing/>,<ContactUs/>,<Footer/>]}/>
+          <Route path="/entrylevel-start-page" element={<Entrylevel/>}/>
+          <Route path="/entrylevel-start-page/quick-assessment" element={<Assessmentsstart/>}/>
+          <Route path="/finish-assessment" element={<Closelevel/>}/>
       </Routes>
-     </Router>
+     </Router>  
     </div>
   );
 }
