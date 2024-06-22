81465dce642f3e4846b0404a59a0122b7f287734
deivasigamanivaradharaj <deivasigamanivaradharaj@gmail.com>
2024-06-22 12:57:45 +0530

    Change to Project File Structure.

--- src/App.js
+++ src/App.js
@@ -2,27 +2,28 @@ import './App.css';
 
 //Bootstrap imported
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 
 //react-router
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 //Landingpage
-import Landingpage from './Components/Landingpage/Landingpage';
-import Management from './Components/Managements/Management';
-import Brands from './Components/Brands/Brands';
-import Footer from './Components/Footer/Footer';
-import ContactUs from './Components/ContactUs/ContactUs';
-import FindPricing from './Components/FindPricing/FindPricing';
-import Partners from './Components/Partners/Partners';
-import Success from './Components/Success/Success';
-import Engage from './Components/Engage/Engage'
-// import Overlaycards from './Components/Overlaycards/Overlaycards'
+import Landingpage from './LandingPage/Landingpage/Landingpage';
+import Management from './LandingPage/Managements/Management';
+import Brands from './LandingPage/Brands/Brands';
+import Footer from './LandingPage/Footer/Footer';
+import ContactUs from './LandingPage/ContactUs/ContactUs';
+import FindPricing from './LandingPage/FindPricing/FindPricing';
+import Partners from './LandingPage/Partners/Partners';
+import Success from './LandingPage/Success/Success';
+import Engage from './LandingPage/Engage/Engage'
+// import Overlaycards from './LandingPage/Overlaycards/Overlaycards'
 
 //Assessments
-import Entrylevel from './Components/Assessments/Entrylevel/Entrylevel';
-import Closelevel from './Components/Assessments/Closelevel/Closelevel';
-import Assessmentsstart from './Components/Assessments/Assessmentsstart/Assessmentsstart'
+import Entrylevel from './Assessments/Entrylevel/Entrylevel';
+import Closelevel from './Assessments/Closelevel/Closelevel';
+import Assessmentsstart from './Assessments/Assessmentsstart/Assessmentsstart'
 
 function App() {
   return (
@@ -30,8 +31,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={[<Landingpage/>,<Management/>,<Brands/>,<Success/>,<Engage/>,<Partners/>,<FindPricing/>,<ContactUs/>,<Footer/>]}/>
-          <Route path="/entrylevel-start-page" element={<Entrylevel/>}/>
-          <Route path="/quick-assessment" element={<Assessmentsstart/>}/>
+          <Route path="/quick-assessment" element={<Entrylevel/>}/>
+          <Route path="/assessment-page" element={<Assessmentsstart/>}/>
           <Route path="/finish-assessment" element={<Closelevel/>}/>
       </Routes>
      </Router>  
